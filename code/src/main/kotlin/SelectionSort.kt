a857d62d9e3b1d03b53cba558e1d34b978ee6de8
kinijyoti <jyotimeher@gmail.com>
2024-12-01 00:06:17 -0600

    selection sort update comment

--- code/src/main/kotlin/SelectionSort.kt
+++ code/src/main/kotlin/SelectionSort.kt
@@ -1,5 +1,9 @@
 /*
-The selection sort algorithm is similar to how humans often sort things. For example, if you're organizing a hand of cards, you can find the lowest value card and put it in the left-most spot. Then, find the lowest of the remaining cards and place it to the right of the first card. Repeat until all the cards are in order.
+The selection sort algorithm is similar to how humans often sort things.
+For example, if you're organizing a hand of cards, you can find the lowest
+value card and put it in the left-most spot.
+Then, find the lowest of the remaining cards
+and place it to the right of the first card. Repeat until all the cards are in order.
 
 The steps of the selection sort algorithm are:
 1. Initialize: Set the first element as the current minimum.
@@ -8,11 +12,10 @@ The steps of the selection sort algorithm are:
 4. Repeat: Repeat steps 2–3 until the entire list is sorted.
 
 Here's a more detailed explanation of the steps:
-Initialize: Start by assuming the first element is the smallest.
-Search: Compare each remaining unsorted element to the current minimum. If a smaller element is found, update the current minimum.
-Swap: After finding the smallest element, swap it with the first unsorted element.
-Repeat: Repeat steps 2–3 until the entire list is sorted.
-The selection sort algorithm is similar to how humans often sort things. For example, if you're organizing a hand of cards, you can find the lowest value card and put it in the left-most spot. Then, find the lowest of the remaining cards and place it to the right of the first card. Repeat until all the cards are in order.
+1. Initialize: Start by assuming the first element is the smallest.
+2. Search: Compare each remaining unsorted element to the current minimum. If a smaller element is found, update the current minimum.
+3. Swap: After finding the smallest element, swap it with the first unsorted element.
+4. Repeat: Repeat steps 2–3 until the entire list is sorted.
 
  */
 
