dbef4c8765f513fc1121b4c0fde264379c87fe62
kinijyoti <jyotimeher@gmail.com>
2024-12-01 00:04:50 -0600

    selection sort

---
+++ code/src/main/kotlin/SelectionSort.kt
@@ -0,0 +1,34 @@
+/*
+The selection sort algorithm is similar to how humans often sort things. For example, if you're organizing a hand of cards, you can find the lowest value card and put it in the left-most spot. Then, find the lowest of the remaining cards and place it to the right of the first card. Repeat until all the cards are in order.
+
+The steps of the selection sort algorithm are:
+1. Initialize: Set the first element as the current minimum.
+2. Search: Find the smallest element in the unsorted section of the list.
+3. Swap: Swap the smallest element with the first element in the unsorted section.
+4. Repeat: Repeat steps 2–3 until the entire list is sorted.
+
+Here's a more detailed explanation of the steps:
+Initialize: Start by assuming the first element is the smallest.
+Search: Compare each remaining unsorted element to the current minimum. If a smaller element is found, update the current minimum.
+Swap: After finding the smallest element, swap it with the first unsorted element.
+Repeat: Repeat steps 2–3 until the entire list is sorted.
+The selection sort algorithm is similar to how humans often sort things. For example, if you're organizing a hand of cards, you can find the lowest value card and put it in the left-most spot. Then, find the lowest of the remaining cards and place it to the right of the first card. Repeat until all the cards are in order.
+
+ */
+
+class SelectionSort {
+    fun selectionSort(array: IntArray) {
+        val n = array.size
+        for (i in 0 until n - 1) {
+            var minIndex = i
+            for (j in i + 1 until n) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j
+                }
+            }
+            val temp = array[i]
+            array[i] = array[minIndex]
+            array[minIndex] = temp
+        }
+    }
+}
