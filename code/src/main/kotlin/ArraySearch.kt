c6599bc3c04c8a55d713dfb9e7793e43e34d6ed1
kinijyoti <jyotimeher@gmail.com>
2024-12-01 18:46:01 -0600

    sorted array search

--- code/src/main/kotlin/ArraySearch.kt
+++ code/src/main/kotlin/ArraySearch.kt
@@ -22,4 +22,20 @@ class ArraySearch {
         }
         return -1 // Return -1 if the element is not found
     }
+
+    fun findElementInSortedArray(array: Array<Int>, target: Int): Int {
+        var left = 0
+        var right = array.size - 1
+
+        while (left <= right) {
+            val mid = left + (right - left) / 2 // Avoid overflow for large indices
+
+            when {
+                array[mid] == target -> return mid // Element found
+                array[mid] < target -> left = mid + 1 // Search in the right half
+                else -> right = mid - 1 // Search in the left half
+            }
+        }
+        return -1 // Element not found
+    }
 }
